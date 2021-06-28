11843815cd45a99ad5c48b4460e763a7d610c979
jlucasoliveira <lucasosilva68@gmail.com>
2021-06-28 14:20:38 -0300

    chore: db connections factories

---
+++ phresh-tutorial/backend/app/db/tasks.py
@@ -0,0 +1,22 @@
+from databases import Database
+from fastapi import FastAPI
+
+from ..core.config import DATABAS_URL
+from ..utils import handle_exception
+
+
+async def connect_to_db(app: FastAPI) -> None:
+    database = Database(DATABAS_URL, min_size=2, max_size=10)
+
+    try:
+        await database.connect()
+        app.state._db = database
+    except Exception as e:
+        handle_exception("DB CONNECTION", e)
+
+
+async def close_db_connection(app: FastAPI) -> None:
+    try:
+        await app.state._db.disconnect()
+    except Exception as e:
+        handle_exception("DB DISCONNECTION", e)
