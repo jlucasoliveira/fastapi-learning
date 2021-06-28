a46c1d01288029a7d45f8e2001461bfceb1c4484
jlucasoliveira <lucasosilva68@gmail.com>
2021-06-28 17:13:05 -0300

    feat: creating route of cleanings

--- phresh-tutorial/backend/app/api/routes/cleanings.py
+++ phresh-tutorial/backend/app/api/routes/cleanings.py
@@ -1,6 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter
+from app.api.dependencies.database import get_repository
+from app.db.repositories.cleanings import CleaningRepository
+from app.models.cleaning import CleaningCreate, CleaningPublic
+from fastapi import APIRouter, Body, Depends, status
 
 router = APIRouter()
 
@@ -12,3 +15,17 @@ async def get_all_cleanings() -> List[dict]:
         {"id": 2, "name": "Someone else's house", "cleaning_type": "spot_clean", "price_per_hour": 19.99},
     ]
     return cleanings
+
+
+@router.post(
+    "/",
+    response_model=CleaningPublic,
+    name="cleanings:create-cleaning",
+    status_code=status.HTTP_201_CREATED,
+)
+async def create_new_cleaning(
+    new_cleaning: CleaningCreate = Body(Ellipsis),
+    cleanings_repo: CleaningRepository = Depends(get_repository(CleaningRepository)),
+) -> CleaningPublic:
+    created_cleaning = await cleanings_repo.create_cleaning(obj=new_cleaning)
+    return created_cleaning
