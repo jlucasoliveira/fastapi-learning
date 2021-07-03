f8064b13526adf8873885a6fed0b6d6fdded2bb0
jlucasoliveira <lucasosilva68@gmail.com>
2021-07-03 17:26:25 -0300

    feat: create retrieve cleaning endpoint

--- phresh-tutorial/backend/app/api/routes/cleanings.py
+++ phresh-tutorial/backend/app/api/routes/cleanings.py
@@ -3,7 +3,7 @@ from typing import List
 from app.api.dependencies.database import get_repository
 from app.db.repositories.cleanings import CleaningRepository
 from app.models.cleaning import CleaningCreate, CleaningPublic
-from fastapi import APIRouter, Body, Depends, status
+from fastapi import APIRouter, Body, Depends, HTTPException, status
 
 router = APIRouter()
 
@@ -15,6 +15,21 @@ async def get_all_cleanings() -> List[dict]:
         {"id": 2, "name": "Someone else's house", "cleaning_type": "spot_clean", "price_per_hour": 19.99},
     ]
     return cleanings
+
+
+@router.get(
+    "/{id}/",
+    response_model=CleaningPublic,
+    name="cleanings:retrieve-cleaning",
+)
+async def retrieve_cleaning(
+    id: int,
+    cleanings_repo: CleaningRepository = Depends(get_repository(CleaningRepository)),
+) -> CleaningPublic:
+    cleaning = await cleanings_repo.retrieve_cleaning(id=id)
+    if not cleaning:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No cleaning found with that id.")
+    return cleaning
 
 
 @router.post(
