3bd6130a063f01cb16bd64fa42c329d00117cac9
jlucasoliveira <lucasosilva68@gmail.com>
2021-07-03 17:25:26 -0300

    tests: creating get tests and parametrizing creating errors

--- phresh-tutorial/backend/tests/test_cleanings.py
+++ phresh-tutorial/backend/tests/test_cleanings.py
@@ -1,5 +1,7 @@
+from typing import Optional
+
 import pytest
-from app.models.cleaning import CleaningCreate, CleaningType
+from app.models.cleaning import CleaningCreate, CleaningInDB, CleaningType
 from fastapi import FastAPI, status
 from httpx import AsyncClient
 
@@ -26,7 +28,7 @@ class TestCleaningsRoutes:
         assert res.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
 
-class TestCleaningCreate:
+class TestCreateCleaning:
     async def test_valid_input_create_cleaning(
         self, app: FastAPI, client: AsyncClient, new_cleaning: CleaningCreate
     ) -> None:
@@ -35,3 +37,42 @@ class TestCleaningCreate:
 
         created_cleaning = CleaningCreate(**res.json())
         assert created_cleaning == new_cleaning
+
+    @pytest.mark.parametrize(
+        "invalid_input, status_code",
+        (
+            (None, 422),
+            ({}, 422),
+            ({"name": "test_name"}, 422),
+            ({"price": 10.0}, 422),
+            ({"name": "test_name", "description": "test"}, 422),
+        ),
+    )
+    async def test_invalid_input_raises_error(
+        self, app: FastAPI, client: AsyncClient, invalid_input: Optional[dict], status_code: int
+    ) -> None:
+        res = await client.post(app.url_path_for("cleanings:create-cleaning"), json=invalid_input)
+        assert res.status_code == status_code
+
+
+class TestGetCleaning:
+    async def test_get_cleaning_by_id(self, app: FastAPI, client: AsyncClient, test_cleaning: CleaningInDB) -> None:
+        res = await client.get(app.url_path_for("cleanings:retrieve-cleaning", id=test_cleaning.id))
+        assert res.status_code == status.HTTP_200_OK
+
+        cleaning = CleaningInDB(**res.json())
+        assert cleaning == test_cleaning
+
+    @pytest.mark.parametrize(
+        "id, status_code",
+        (
+            (None, 422),
+            (99, 404),
+            (-99, 404),
+        ),
+    )
+    async def test_get_cleaning_raises_error(
+        self, app: FastAPI, client: AsyncClient, id: Optional[int], status_code: int
+    ) -> None:
+        res = await client.get(app.url_path_for("cleanings:retrieve-cleaning", id=id))
+        assert res.status_code == status_code
