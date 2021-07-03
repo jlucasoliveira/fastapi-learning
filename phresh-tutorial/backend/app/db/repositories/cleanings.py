1fa3982e120d1f4e7ac7fb7b171a394c28f2c4e5
jlucasoliveira <lucasosilva68@gmail.com>
2021-07-03 17:27:36 -0300

    feat: create retrieve cleaning by id on repository

--- phresh-tutorial/backend/app/db/repositories/cleanings.py
+++ phresh-tutorial/backend/app/db/repositories/cleanings.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from app.models.cleaning import CleaningCreate, CleaningInDB
 
 from .base import BaseRepository
@@ -5,7 +7,14 @@ from .base import BaseRepository
 CREATE_CLEANING_QUERY = """
     INSERT INTO cleanings (name, description, price, cleaning_type)
     VALUES (:name, :description, :price, :cleaning_type)
-    RETURNING id, name, description, price, cleaning_type
+    RETURNING id, name, description, price, cleaning_type;
+"""
+
+
+RETRIEVE_CLEANING_BY_ID_QUERY = """
+    SELECT id, name, description, price, cleaning_type
+    FROM cleanings
+    WHERE id = :id;
 """
 
 
@@ -16,5 +25,16 @@ class CleaningRepository(BaseRepository):
             query=CREATE_CLEANING_QUERY,
             values=query_values,
         )
+
+        return CleaningInDB(**cleaning)
+
+    async def retrieve_cleaning(self, *, id: int) -> Union[CleaningInDB, None]:
+        cleaning = await self.db.fetch_one(
+            query=RETRIEVE_CLEANING_BY_ID_QUERY,
+            values={"id": id},
+        )
+
+        if not cleaning:
+            return None
 
         return CleaningInDB(**cleaning)
